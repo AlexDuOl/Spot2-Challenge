ddc2e4a3a2214986fe8e66577f1468d8d93120e9
AlexDuOl <alexdurancm@gmail.com>
2022-06-29 14:17:16 -0500

    Updates

---
+++ precache-manifest.9ce8e4675749cdb2ef99fbbc8031ddbd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "32f40948b94c46e798c7",
+    "url": "/Spot2-Challenge/static/css/main.6b77ab81.chunk.css"
+  },
+  {
+    "revision": "32f40948b94c46e798c7",
+    "url": "/Spot2-Challenge/static/js/main.9d9a481e.chunk.js"
+  },
+  {
+    "revision": "0d313fc0c4ad53663482",
+    "url": "/Spot2-Challenge/static/js/runtime~main.cc941a94.js"
+  },
+  {
+    "revision": "b412195294920e9ebdb8",
+    "url": "/Spot2-Challenge/static/css/2.a52d069f.chunk.css"
+  },
+  {
+    "revision": "b412195294920e9ebdb8",
+    "url": "/Spot2-Challenge/static/js/2.2adbdc22.chunk.js"
+  },
+  {
+    "revision": "37fb15046004f2131d1f6bcf4c99d76b",
+    "url": "/Spot2-Challenge/static/media/logo.37fb1504.png"
+  },
+  {
+    "revision": "253b809c88228b21798d63e02cb057c6",
+    "url": "/Spot2-Challenge/index.html"
+  }
+];
